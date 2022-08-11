fa570d88870f990f930dae08030e8004d5a83dc2
Aniket Kale <aniketkale199200@gmail.com>
2022-08-11 10:50:32 +0530

    initial

--- lib/models/db.dart
+++ lib/models/db.dart
@@ -11,27 +11,21 @@ class TodoDatabase extends ChangeNotifier {
 
   static const table = "my_table";
 
-  // column names
   static const columnID = 'id';
   static const columnName = "todo";
 
-  // a database
   static Database? _database;
 
-  // privateconstructor
   TodoDatabase._privateConstructor();
   static final TodoDatabase instance = TodoDatabase._privateConstructor();
 
-  // asking for a database
   Future<Database?> get databse async {
     if (_database != null) return _database;
 
-    // create a database if one doesn't exist
     _database = await _initDatabase();
     return _database;
   }
 
-  // function to return a database
   _initDatabase() async {
     Directory documentdirecoty = await getApplicationDocumentsDirectory();
     String path = join(documentdirecoty.path, _databasename);
@@ -39,9 +33,7 @@ class TodoDatabase extends ChangeNotifier {
         version: _databaseversion, onCreate: _onCreate);
   }
 
-  // create a database since it doesn't exist
   Future _onCreate(Database db, int version) async {
-    // sql code
     await db.execute('''
       CREATE TABLE $table (
         $columnID INTEGER PRIMARY KEY,
@@ -50,7 +42,6 @@ class TodoDatabase extends ChangeNotifier {
       ''');
   }
 
-  // functions to insert data
   Future<int> insert(Map<String, dynamic> row) async {
     Database? db = await instance.databse;
     int res = await db!.insert(table, row);
@@ -58,19 +49,16 @@ class TodoDatabase extends ChangeNotifier {
     return res;
   }
 
-  // function to query all the rows
   Future<List<Map<String, dynamic>>> queryall() async {
     Database? db = await instance.databse;
     return await db!.query(table);
   }
 
-  // function to query all the rows
   Future<Map<String, dynamic>> queryone(int id) async {
     Database? db = await instance.databse;
     return (await db!.query(table, where: "id = ?", whereArgs: [id])).first;
   }
 
-  // function to delete some data
   Future<int> deletedata(int id) async {
     Database? db = await instance.databse;
     int res = await db!.delete(table, where: "id = ?", whereArgs: [id]);
